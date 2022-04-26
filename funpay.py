5d3e392436f81a08221f07c2f396ce31200e5adb
MRossa <maximrossa1@gmail.com>
2022-04-26 21:46:26 +0400

    Rewritten the concept of work, added a time counter, code optimization

--- funpay.py
+++ funpay.py
@@ -1,7 +1,17 @@
 from selenium import webdriver
+from selenium.common.exceptions import SessionNotCreatedException
 from selenium.common.exceptions import NoSuchElementException
+from sys import exit
 import time
 import pickle
+
+
+def UpdateChromeDriver():
+    chromeVersion = SessionNotCreated[SessionNotCreated.find('Current browser version is'):SessionNotCreated.find('with binary')]
+    chromeVersion = chromeVersion[len('Current browser version is '):len(chromeVersion)-1]
+    chromeUrl = f'https://chromedriver.storage.googleapis.com/{chromeVersion}/chromedriver_win32.zip'
+    print('Please follow this link and unzip chromedriver.exe into your application folder:')
+    print(chromeUrl)
 
 def check_exist_by_xpath(xpath):
     try:
@@ -9,93 +19,93 @@ def check_exist_by_xpath(xpath):
         return True
     except NoSuchElementException:
         return False
-
-def check_exist_by_css(css):
-    try:
-        elem = driver.find_element_by_css_selector(css)
-        return True
-    except NoSuchElementException:
-        return False
     
-def activating_order():
-    driver.find_element_by_css_selector('.tc-item.warning').click()
-    driver.find_element_by_xpath("//label[contains(text(),'Активное')]").click()
-    driver.find_element_by_xpath("//button[contains(text(),'Сохранить')]").click()
-    driver.refresh()
-    time.sleep(1)
-    print('Активировали товар')
-
-def raise_order(site, number_buttons, game):
-    driver.get(site)
-    if check_exist_by_css('.tc-item.warning') and game != 'Dota 2' and game != 'WoT':
-        activating_order()
-    driver.find_element_by_xpath("//button[contains(text(),'Поднять предложения')]").click()
-    if not check_exist_by_xpath("//div[@class='ajax-alert ajax-alert-danger']"):        
-        if game.find("Crossout") or game.find('War Thunder'):
+def raiseOrder():
+    driver.find_element_by_xpath("//button[@class='btn btn-default btn-block js-lot-raise']").click()
+    if not check_exist_by_xpath("//div[@class='ajax-alert ajax-alert-danger']"):
+        rid = 0
+        while True:
+            rid += 1
             try:
-                driver.find_element_by_xpath("//button[@class='btn btn-primary btn-block js-lot-raise-ex']").click()
-            except NoSuchElementException:
-                pass
-        print(f'{game}: Успешно поднят!')
-
-
+                if not driver.find_element_by_xpath(f"/html/body/div[2]/div/div/div[2]/div/div[{rid}]/label/input").is_selected():
+                    driver.find_element_by_xpath(f"/html/body/div[2]/div/div/div[2]/div/div[{rid}]/label").click()
+            except Exception:
+                break
+        driver.find_element_by_xpath(f"//button[@class='btn btn-primary btn-block js-lot-raise-ex']").click()
+    else:
+        return 0
+    
 def main():
     driver.implicitly_wait(2)
     try:  
-        driver.get("https://funpay.ru/account/login")
-        print('Перешли на страницу логина')
-        for cookie in pickle.load(open("fp_cookies_new", "rb")):
-            driver.add_cookie(cookie)  
+        driver.get('https://funpay.ru/account/login')
+
+        for cookie in pickle.load(open('cookie', 'rb')):
+            driver.add_cookie(cookie)
         driver.refresh()
-        print('Залогинились')
+        print('[Prog] Вошёл в аккаунт')
+        #delay = int(input('[Prog] Введите вашу задержку (в минутах)')) # отголоски интерфейса
+        delay = 10 * 60 # стандарт 10 минут
+        driver.get('https://funpay.ru/users/3302780/')      # тут ссылка на свой аккаунт
         timer_min = 0
         timer_hour = 0
         timer_string = '0 часов 0 минут'
         while True:
-            print('Проверяем Crossout')
-            raise_order("https://funpay.ru/lots/213/trade",1,'Crossout [1/2]')
-            raise_order("https://funpay.ru/lots/214/trade",1,'Crossout [2/2]')
-            print('Проверяем War Thunder')
-            raise_order("https://funpay.ru/lots/244/trade",1,'War Thunder [1/2]')
-            raise_order("https://funpay.ru/lots/746/trade",1,'War Thunder [2/2]')
-            print('Проверяем Star Conflict')
-            raise_order("https://funpay.ru/lots/423/trade",1,'Star Conflict')
-            print(f'Ждём 10 минут (Всего работаем: {timer_string})')    
-            time.sleep(600)
-            timer_min += 10
+            i = 1
+            print('[Prog] Начинаю проход по лотам')
+            while True:
+                i += 1
+                try:
+                    driver.find_element_by_xpath(f"(//i[@class='fa fa-pen'])[{i}]").click()
+                    raiseOrder()
+                    driver.get('https://funpay.ru/users/3302780/')
+                except NoSuchElementException:
+                    break
+            pickle.dump(driver.get_cookies(), open('cookie', 'wb')) # сохранение куки
+            print(f'Жду {delay//60} минут (Всего работаю: {timer_string})')
+            time.sleep(delay)
+            timer_min += delay / 60
             if timer_min % 60 == 0:
                 timer_min = 0
                 timer_hour += 1
             timer_string = f'{timer_hour} часов, {timer_min} минут'
- 
-
-    
+            
     except Exception as ex:
         print(ex)
     finally:
-        driver.close()
         driver.quit()
 
+
 ###########START WEB DRIVER###########
-#options
-options = webdriver.ChromeOptions()
+try:
+    #options
+    options = webdriver.ChromeOptions()
 
-#change user-agent
-options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36")
+    #change user-agent
+    options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36")
 
-#headless mode
-options.add_argument("--headless")
+    #headless mode
+    #options.add_argument("--headless")
 
-#disable webdriver mode
-options.add_argument("--disable-blink-features=AutomationControlled")
+    #disable webdriver mode
+    options.add_argument("--disable-blink-features=AutomationControlled")
 
-#disable automation flag
-options.add_experimental_option("useAutomationExtension", False)
-options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    #disable automation flag
+    options.add_experimental_option("useAutomationExtension", False)
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
 
-driver = webdriver.Chrome(options = options)
+    driver = webdriver.Chrome(options = options)
+    
+except SessionNotCreatedException as SessionNotCreated:
+        SessionNotCreated = str(SessionNotCreated)
+        if SessionNotCreated.find('This version of ChromeDriver only supports'):
+            print('Your version chromedriver is out of date.')
+            UpdateChromeDriver()
+            exit()
+            
 
 ########################################
 
 if __name__ == '__main__':
     main()
+    
