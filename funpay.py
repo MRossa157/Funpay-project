f61d8a304b73e9059294295c002cbe3cdf74b586
MRossa <maximrossa1@gmail.com>
2022-04-26 21:47:43 +0400

    Adding auto-activation of already purchased orders, rebranding

--- funpay.py
+++ funpay.py
@@ -19,9 +19,34 @@ def check_exist_by_xpath(xpath):
         return True
     except NoSuchElementException:
         return False
+
+def check_exist_by_css(css):
+    try:
+        elem = driver.find_element_by_css_selector(css)
+        return True
+    except NoSuchElementException:
+        return False    
+
+def autoActivateOrderFunction():
+        driver.find_element_by_css_selector('.tc-item.warning').click()
+        if not driver.find_element_by_xpath(f"/html[1]/body[1]/div[2]/div[1]/div[1]/div[2]/form[1]/div[6]/div[1]/label[1]/i[1]").is_selected():
+            driver.find_element_by_xpath(f"/html[1]/body[1]/div[2]/div[1]/div[1]/div[2]/form[1]/div[6]/div[1]/label[1]/i[1]").click()
+        driver.find_element_by_xpath("/html[1]/body[1]/div[2]/div[1]/div[1]/div[2]/form[1]/div[7]/button[1]").click()
+        print('[FPA] Активировал лот')
+        time.sleep(0.25)
+
+def firstStart(array):
+    time.sleep(0.1)
+    array.append(driver.current_url)
+    return array
     
-def raiseOrder():
-    driver.find_element_by_xpath("//button[@class='btn btn-default btn-block js-lot-raise']").click()
+    
+def raiseOrder(flag):
+    if check_exist_by_css('.tc-item.warning') and flag:
+        print('вошли')
+        autoActivateOrderFunction()
+    driver.find_element_by_xpath("//button[contains(text(),'Поднять предложения')]").click()
+    #{!} не рассмотрен случай когда нажимается кнопка только на 1 товар(без списка то есть)
     if not check_exist_by_xpath("//div[@class='ajax-alert ajax-alert-danger']"):
         rid = 0
         while True:
@@ -32,39 +57,47 @@ def raiseOrder():
             except Exception:
                 break
         driver.find_element_by_xpath(f"//button[@class='btn btn-primary btn-block js-lot-raise-ex']").click()
-    else:
-        return 0
+    return 0
     
 def main():
+    print('FunPay Assistant by MRossa')
+    #delay = int(input('[FPA] Введите вашу задержку (в минутах)')) # отголоски интерфейса
+    delay = 10 * 60 # стандарт 10 минут
+    autoActivateOrder = True
+    FirtsStartFlag = True
+    links = []
     driver.implicitly_wait(2)
     try:  
         driver.get('https://funpay.ru/account/login')
-
         for cookie in pickle.load(open('cookie', 'rb')):
             driver.add_cookie(cookie)
         driver.refresh()
-        print('[Prog] Вошёл в аккаунт')
-        #delay = int(input('[Prog] Введите вашу задержку (в минутах)')) # отголоски интерфейса
-        delay = 10 * 60 # стандарт 10 минут
+        print('[FPA] Вошёл в аккаунт')
         driver.get('https://funpay.ru/users/3302780/')      # тут ссылка на свой аккаунт
         timer_min = 0
         timer_hour = 0
         timer_string = '0 часов 0 минут'
         while True:
-            i = 1
-            print('[Prog] Начинаю проход по лотам')
-            while True:
-                i += 1
-                try:
-                    driver.find_element_by_xpath(f"(//i[@class='fa fa-pen'])[{i}]").click()
-                    raiseOrder()
-                    driver.get('https://funpay.ru/users/3302780/')
-                except NoSuchElementException:
-                    break
+            if FirtsStartFlag:
+                i = 1
+                print('[FPA] Начинаю проход по лотам')
+                while True:
+                    i += 1
+                    try:
+                        driver.find_element_by_xpath(f"(//i[@class='fa fa-pen'])[{i}]").click()
+                        firstStart(links)
+                        driver.get('https://funpay.ru/users/3302780/')
+                    except NoSuchElementException:
+                        break
+                print(links,' ', len(links))
+                FirtsStartFlag = False
+            for url in links:
+                driver.get(url)
+                raiseOrder(autoActivateOrder)
             pickle.dump(driver.get_cookies(), open('cookie', 'wb')) # сохранение куки
-            print(f'Жду {delay//60} минут (Всего работаю: {timer_string})')
+            print(f'[FPA] Жду {delay//60} минут (Всего работаю: {timer_string})')
             time.sleep(delay)
-            timer_min += delay / 60
+            timer_min += delay // 60
             if timer_min % 60 == 0:
                 timer_min = 0
                 timer_hour += 1
